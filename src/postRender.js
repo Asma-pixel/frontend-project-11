44264b7519f48248a786798d943f210b6de59457
seraphim <serafimcomru91@gmail.com>
2022-12-11 20:24:37 +0300

    add css class

--- src/postRender.js
+++ src/postRender.js
@@ -13,7 +13,7 @@ export default ({ content, uiState }, elements) => {
   content.posts.forEach((item) => {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
-    const a = `<a data-id=${item.id} href="${item.url}">${item.title}</a>`;
+    const a = `<a data-id=${item.id} class="fw-bold"href="${item.url}">${item.title}</a>`;
     li.innerHTML = a;
     const button = document.createElement('button');
     button.type = 'button';
